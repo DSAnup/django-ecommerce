9bade7500b38c099e96bf4236e799bb538d807c4
dsanup <anup12.m@gmail.com>
2023-09-01 22:48:50 +0600

    first model & choice flieds

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    Title = models.CharField(max_length=255)
+    Description = models.TextField()
+    Price = models.DecimalField(
+        max_digits=6, decimal_places=2
+    )  # defined the max digits & decimal digits
+    Inventory = models.IntegerField()
+    LastUpdate = models.DateTimeField(auto_now=True)
+
+
+class Customer(models.Model):
+    MemberShipBronze = "B"
+    MemberShipSilver = "S"
+    MemberShipGold = "G"
+    MemberShipChoices = [
+        (MemberShipBronze, "Bronze"),
+        (MemberShipSilver, "Silver"),
+        (MemberShipGold, "Gold"),
+    ]
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    Email = models.EmailField(unique=True)
+    Phone = models.CharField(max_length=100)
+    BirthDate = models.DateField(null=True)
+    MemberShip = models.CharField(
+        max_length=1, choices=MemberShipChoices, default=MemberShipBronze
+    )
+
+
+class Order(models.Model):
+    PaymentStatusPending = "P"
+    PaymentStatusComplete = "C"
+    PaymentStatusFailed = "F"
+    PaymentStatusChoices = [
+        (PaymentStatusPending, "Pending"),
+        (PaymentStatusComplete, "Complete"),
+        (PaymentStatusFailed, "Failed"),
+    ]
+    PlacedAt = models.DateTimeField(auto_now_add=True)
+    PaymentStatus = models.CharField(
+        max_length=1, choices=PaymentStatusChoices, default=PaymentStatusPending
+    )
