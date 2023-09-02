b3f64d01aed545abe9db65082d5978924eab976d
dsanup <anup12.m@gmail.com>
2023-09-02 17:07:13 +0600

    modify models name & tag models

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -4,17 +4,17 @@ from django.db import models
 
 
 class Product(models.Model):
-    Title = models.CharField(max_length=255)
-    Description = models.TextField()
-    Price = models.DecimalField(
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(
         max_digits=6, decimal_places=2
     )  # defined the max digits & decimal digits
-    Inventory = models.IntegerField()
-    LastUpdate = models.DateTimeField(auto_now=True)
-    Collection = models.ForeignKey(
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(
         "Collection", on_delete=models.PROTECT
     )  # do not others production
-    Promotions = models.ManyToManyField("Promotion")
+    promotions = models.ManyToManyField("Promotion")
 
 
 class Customer(models.Model):
@@ -26,12 +26,12 @@ class Customer(models.Model):
         (MemberShipSilver, "Silver"),
         (MemberShipGold, "Gold"),
     ]
-    FirstName = models.CharField(max_length=100)
-    LastName = models.CharField(max_length=100)
-    Email = models.EmailField(unique=True)
-    Phone = models.CharField(max_length=100)
-    BirthDate = models.DateField(null=True)
-    MemberShip = models.CharField(
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(
         max_length=1, choices=MemberShipChoices, default=MemberShipBronze
     )
 
@@ -46,43 +46,43 @@ class Order(models.Model):
         (PaymentStatusFailed, "Failed"),
     ]
 
-    Customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
-    PlacedAt = models.DateTimeField(auto_now_add=True)
-    PaymentStatus = models.CharField(
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
         max_length=1, choices=PaymentStatusChoices, default=PaymentStatusPending
     )
 
 
 class Address(models.Model):
-    Customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    Street = models.CharField(max_length=255)
-    City = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
 
 
 class Collection(models.Model):
-    Title = models.CharField(max_length=255)
-    FeaturedProduct = models.ForeignKey(
+    title = models.CharField(max_length=255)
+    feature_product = models.ForeignKey(
         Product, on_delete=models.SET_NULL, null=True, related_name="+"
     )
 
 
 class OrderItem(models.Model):
-    Order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    Product = models.ForeignKey(Product, on_delete=models.PROTECT)
-    Quantity = models.PositiveSmallIntegerField()
-    UnitPrice = models.DecimalField(max_digits=6, decimal_places=2)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 class Cart(models.Model):
-    CreatedAt = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CartItem(models.Model):
-    Cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    Product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    Quantity = models.PositiveSmallIntegerField()
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
 
 
 class Promotion(models.Model):
-    Description = models.CharField(max_length=255)
-    Discount = models.FloatField()
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
