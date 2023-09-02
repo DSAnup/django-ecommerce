43c53b3fb1bb8cab2179f866ca070d4b7b792c58
dsanup <anup12.m@gmail.com>
2023-09-02 17:32:48 +0600

    make migrations

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -6,9 +6,10 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(
+    unit_price = models.DecimalField(
         max_digits=6, decimal_places=2
     )  # defined the max digits & decimal digits
+    slug = models.SlugField()
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(
