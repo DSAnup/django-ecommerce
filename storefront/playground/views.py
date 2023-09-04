e2c687dff17681b0be1f1573f84d35d794072697
dsanup <anup12.m@gmail.com>
2023-09-04 11:48:50 +0600

    filter exists

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 # Create your views here.
 
 
 def say_hello(request):
+    query_set = Product.objects.all()
+    print(query_set)
+    # pk is set as primary key
+    productExists = Product.objects.filter(pk=0).exists()
+    # return None
+    print(productExists)
     return render(request, "hello.html", {"name": "Anup"})
