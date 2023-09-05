ad5549bc98e5e38fbeea47c4490cc2cb38128f33
dsanup <anup12.m@gmail.com>
2023-09-05 10:16:09 +0600

    get min, max ...

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, Order, OrderItem
+from django.db.models import Min, Max, Avg, Count, Sum
 
 # Create your views here.
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
-    print(query_set)
-    # pk is set as primary key
-    productExists = Product.objects.filter(pk=0).exists()
-    # return None
-    print(productExists)
-    return render(request, "hello.html", {"name": "Anup"})
+    queryset = Product.objects.filter(collection__title="Beauty").filter(
+        title__icontains="coffee"
+    )
+    orderCount = Order.objects.aggregate(count=Count("id"))
+    product1Sold = OrderItem.objects.filter(product__id=1).aggregate(
+        units_sold=Sum("quantity")
+    )
+    customer1Order = Order.objects.filter(customer__id=1).aggregate(count=Count("id"))
+    minPriceCollection3 = Product.objects.filter(collection__id=3).aggregate(
+        min_value=Min("unit_price"),
+        max_value=Max("unit_price"),
+        avg_value=Avg("unit_price"),
+    )
+    return render(
+        request,
+        "hello.html",
+        {
+            "name": "Anup",
+            "products": list(queryset),
+            "totalOrder": orderCount,
+            "product1Sold": product1Sold,
+            "customer1Order": customer1Order,
+            "minPriceCollection3": minPriceCollection3,
+        },
+    )
