ea5e3cdda5efaa49f88d814c47fdaeec08b73532
dsanup <anup12.m@gmail.com>
2023-09-17 16:45:15 +0600

    search startswith

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -43,8 +43,12 @@ class CollectionAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "membership", "orders_count"]
     list_editable = ["membership"]
-    ordering = ["first_name", "last_name"]
     list_per_page = 10
+    ordering = ["first_name", "last_name"]
+    search_fields = [
+        "first_name",
+        "last_name__istartswith",
+    ]  # i represent insensitive case
 
     @admin.display(ordering="orders_count")
     def orders_count(self, customer):
