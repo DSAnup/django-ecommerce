83cafca8063ebd71b30f233d97387d9e966dbc37
dsanup <anup12.m@gmail.com>
2023-09-17 16:35:31 +0600

    link to product pages

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from . import models
-from django.db.models import Count
+from django.db.models.aggregates import Count
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 
 
 @admin.register(models.Product)
@@ -26,7 +28,12 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering="products_count")
     def products_count(self, collection):
-        return collection.products_count
+        url = (
+            reverse("admin:store_product_changelist")
+            + "?"
+            + urlencode({"collection__id": str(collection.id)})
+        )
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(products_count=Count("product"))
