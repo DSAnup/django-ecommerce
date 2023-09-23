49b930fec43769ed3d33858d7b760322c068f6be
dsanup <anup12.m@gmail.com>
2023-09-23 21:15:29 +0600

    validate data

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -14,16 +14,7 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ["id", "title", "unit_price", "price_with_tax", "collection"]
 
-    # id = serializers.IntegerField()
-    # title = serializers.CharField(max_length=255)
-    # price = serializers.DecimalField(
-    #     max_digits=6, decimal_places=2, source="unit_price"
-    # )
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-    collection = serializers.StringRelatedField()
-    # collection = serializers.HyperlinkedRelatedField(
-    #     queryset=Collection.objects.all(), view_name="collection-detail"
-    # )
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -17,6 +18,8 @@ def product_list(request):
         return Response(serializer.data)
     elif request.method == "POST":
         serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data
         return Response("Ok")
 
 
