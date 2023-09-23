e7e527256df25b196898d62c920823aefdb886bd
dsanup <anup12.m@gmail.com>
2023-09-23 16:30:08 +0600

    serializing relationships

--- storefront/store/urls.py
+++ storefront/store/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("products/", views.product_list),
     path("products/<int:id>", views.product_details),
+    path("collections/<int:pk>", views.collections_details, name="collection-detail"),
 ]
