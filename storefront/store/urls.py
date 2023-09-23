ae3875d88284b5c67851c70c080b0e1b3fb24d20
dsanup <anup12.m@gmail.com>
2023-09-23 15:55:06 +0600

    creating & view serializer data

--- storefront/store/urls.py
+++ storefront/store/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 # URLConf
-urlpatterns = [path("products/", views.product_list)]
-urlpatterns = [path("products/<int:id>", views.product_details)]
+urlpatterns = [
+    path("products/", views.product_list),
+    path("products/<int:id>", views.product_details),
+]
