b4151c14b6b9c246281b904c3b70f2584191abfc
dsanup <anup12.m@gmail.com>
2023-09-26 21:35:00 +0600

    Use of ModelViewSet

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework import status
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
@@ -11,17 +10,12 @@ from django.db.models.aggregates import Count
 # Create your views here.
 
 
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related("collection").all()
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {"request": self.request}
-
-
-class ProductDetials(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
 
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
@@ -36,13 +30,8 @@ class ProductDetials(RetrieveUpdateDestroyAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count("products")).all()
-    serializer_class = CollectionSerializer
-
-
-class CollectionDetails(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.all()
     serializer_class = CollectionSerializer
 
     def delete(self, request, pk):
