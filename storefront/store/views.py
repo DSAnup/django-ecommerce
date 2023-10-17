15fc6d793cc132cfa33294808d1c7ca044dd97bf
dsanup <anup12.m@gmail.com>
2023-10-17 22:35:12 +0600

    updating orders

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -115,7 +115,12 @@ class CustomerViewSet(ModelViewSet):
 
 
 class OrderViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    http_method_names = ["get", "patch", "delete", "head", "options"]
+
+    def get_permissions(self):
+        if self.request.method in ["DELETE", "PATCH"]:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
 
     def create(self, request, *args, **kwargs):
         serializer = CreateOrderSerializer(
@@ -129,6 +134,8 @@ class OrderViewSet(ModelViewSet):
     def get_serializer_class(self):
         if self.request.method == "POST":
             return CreateOrderSerializer
+        elif self.request.method == "PATCH":
+            return UpdateOrderSerializer
         return OrderSerializer
 
     def get_queryset(self):
