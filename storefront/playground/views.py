c5062bd845b65a906637c12eb6bae9a2722c3c56
dsanup <anup12.m@gmail.com>
2023-12-04 20:05:38 +0600

    collectstatic, log ...

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -9,11 +9,32 @@ from django.core.cache import cache
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework.views import APIView
+import logging
 import requests
+
+logger = logging.getLogger(__name__)
 
 
 # Create your views here.
 class HelloView(APIView):
+    def get(self, request):
+        try:
+            logger.info("Calling httpbin")
+            response = requests.get("https://httpbin.org/delay/2")
+            logger.info("Recieved the response")
+            data = response.json()
+        except requests.ConnectionError:
+            logger.critical("httpbin is offline")
+        return render(
+            request,
+            "hello.html",
+            {
+                "name": "Anup",
+            },
+        )
+
+
+class HelloView2(APIView):
     @method_decorator(cache_page(3 * 60))
     def get(self, request):
         response = requests.get("https://httpbin.org/delay/2")
